34f7a2d53656d3de2cbc71d28da13924da33e027
gioGats <ryangiarusso@gmail.com>
2017-01-25 14:16:09 -0600

    Interfaces for CLI and Classifier

--- CLI.py
+++ CLI.py
@@ -1,1 +1,20 @@
 # FUTURE Command-Line Interface for running sub-scripts
+
+
+class CLI(object):
+    def __init__(self):
+        pass
+
+
+class Classifier(object):
+    def __init__(self):
+        pass
+
+    def classify(self, dicom_list):
+        """
+        Given a list of dicom images from a patient,
+        return probability that patient is diagnosed with lung cancer in the next 12 months.
+        :param dicom_list: list of dicom images
+        :return: float in range [0,1]
+        """
+        raise NotImplementedError
