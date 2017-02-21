3f3b7a558e256a684bb1d0b8c44dd7f7a165f23b
gioGats <ryangiarusso@gmail.com>
2017-02-21 13:28:43 -0600

    Documentation

--- fritz/candidates/cnn.py
+++ fritz/candidates/cnn.py
@@ -1,7 +1,16 @@
 import tflearn
 
 
-def fully_connected(input_shape, layers, optimizer, loss):
+def fully_connected(input_shape, layers, optimizer='adam', loss='categorical_crossentropy'):
+    """
+    Make a fully-connected neural network based on the input parameters
+    :param input_shape: a list that when added to [None] describes the tensor dimensions of input data
+    :param layers: a list where each entry describes a layer as a list of
+            [number_of_nodes, probability_of_dropout, activation_function]
+    :param optimizer: str of optimizer to use
+    :param loss: str of loss function to use
+    :return: a tflearn.DNN object
+    """
     tflearn.init_graph()
     net = tflearn.input_data(shape=[None] + input_shape)
     for l in layers:
