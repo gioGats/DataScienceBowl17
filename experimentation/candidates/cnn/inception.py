01444554dfff44fa86f9730db8f0a08a20260d7b
gioGats <ryangiarusso@gmail.com>
2017-02-28 12:14:09 -0600

    CNN model interface

---
+++ experimentation/candidates/cnn/inception.py
@@ -0,0 +1,11 @@
+import tflearn
+
+
+def inception_model(input_tensor=[None, 512], output_tensor=[None, 1]):
+    """
+    Generates an inception model that conforms to specified input/output tensors.
+    :param input_tensor: Dimensions of input
+    :param output_tensor: Dimensions of output
+    :return: An instance of tflearn.DNN class supporting {fit, predict, evaluate, save, load} methods
+    """
+    pass
