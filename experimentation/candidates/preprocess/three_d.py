528cc2b52798c868bb1d2867b21cf6efdb2c68dc
gioGats <ryangiarusso@gmail.com>
2017-03-11 09:51:42 -0600

    Simple initial preprocessing function

--- experimentation/candidates/preprocess/three_d.py
+++ experimentation/candidates/preprocess/three_d.py
@@ -1,53 +1,46 @@
+import numpy as np
+from skimage.transform import resize
+from data_manipulation_1 import load_scan
+
+"""
+=================================================================================
+For this branch (modular_preprocess), I have arbitrarily selected skimage as the
+image preprocessing library.  It has unbelievably worse time performance compared
+to openCV (11s vs 770s).  But it has much superior documentation and supports 3D
+resizing natively.  We should strongly consider an openCV model to speed this up.
+=================================================================================
+"""
 
 
 def three_d_preprocess(dicom_directory,
-                       x=512, y=512, slices=100, channels=1, upsample='', downsample='', processing=''):
-    # TODO Use str params for upsample, downsample, processing to assign functions to the following variables:
-    # upsample_2d_function
-    # upsample_3d_function
-    # downsample_2d_function
-    # downsample_3d_function
-    # processing_function
+                       x=512, y=512, slices=100,
+                       mode=None, processing=''):
+    # Validate input, passed directly to skimage.transform.resize()
+    if (mode is not None) and \
+            (mode not in ['constant', 'edge', 'symmetric', 'reflect', 'wrap']):
+        raise NotImplementedError('%s not a valid resizing mode' % mode)
 
-    # TODO Bring all dicoms in dicom_directory into memory
+    # TODO Use str param for processing to specify more processing functions
+    if processing == '' or 'hu':
+        from data_manipulation_1 import get_pixels_hu as processing
 
-    # TODO Process raw data values into a 2D numpy array (z, (x, y, channel))
+    # Bring all dicoms in dicom_directory into memory, and
+    # process raw data values into a 2D numpy array (z, (x, y, channel))
+    # with pixel values adjusted with processing function (defined above)
+    patient_array = processing(load_scan(dicom_directory))
 
-    # TODO Process metadata
+    # TODO Get patient label
+    # label = get_label(dicom_directory.split('/')[-1])
+    label = 1
 
-    # TODO Apply processing function
-    #     processed_array = processing_function(resized_array)
-    # TODO Replace slice_array with processed_array
-    # (In the same data structure? This will be a single threaded operation, so priority to speed)
+    # Apply 2D resizing
+    if slices <= 0:
+        patient_array = resize(patient_array, (x, y, len(patient_array)), mode=mode)
+    # Apply 3D resizing
+    else:
+        patient_array = resize(patient_array, (x, y, slices), mode=mode)
+    return np.array([patient_array, label])
 
-    # TODO Apply 2D resizing
-    # for slice_array in slices:
-    #     if (slice_array.x_shape < x) or (slice_array.y_shape < y) or (slice_array.channel_shape < channels):
-    #         resized_array = upsample_2d_function(slice_array)
-    #     elif (slice_array.x_shape > x) or (slice_array.y_shape > y) or (slice_array.channel_shape > channels):
-    #         resized_array = downsample_2d_function(slice_array)
-    #     else:
-    #         raise Error Case
-
-
-    # TODO 2D quick exit
-    # if slices <= 0:
-    # TODO Convert 3D array to 2D array
-    # [(slice_array, label), (slice_array, label), (slice_array, label), (slice_array, label)]
-    #    reshaped_array = resize_2d(slice_array)
-    # TODO Ouput 2D array
-    #     return reshaped_array
-
-    # TODO Apply 3D resizing
-    # else:
-    #     if len(slice_array) < slices:
-    #         resized_cube = upsample_3d_function(slice_array)
-    #     elif len(slice_array) > slices:
-    #         resized_cube = downsample_3d_function(slice_array)
-    #     else:
-    #         resized_cube = slice_array
-    # TODO Output labelled 3D array
-    # [resized_cube, label]
-    # [(slice_array, slice_array, slice_array, slice_array), label]
-    pass
-
+if __name__ == '__main__':
+    test_individual = '/nvme/stage1_data/sample_images/0a0c32c9e08cc2ea76a71649de56be6d'
+    three_d_preprocess(test_individual, slices=-1)
