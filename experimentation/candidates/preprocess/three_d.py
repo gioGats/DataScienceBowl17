7cd9713caae075e14371cafc6f9cd15eabb8a6d6
gioGats <ryangiarusso@gmail.com>
2017-03-11 12:44:01 -0600

    Merge data_manipulation_1 except blurring code

--- experimentation/candidates/preprocess/three_d.py
+++ experimentation/candidates/preprocess/three_d.py
@@ -1,6 +1,5 @@
 import numpy as np
 from skimage.transform import resize
-from data_manipulation_1 import load_scan
 
 """
 =================================================================================
@@ -31,13 +30,13 @@ def three_d_preprocess(dicom_directory,
             (mode not in ['constant', 'edge', 'symmetric', 'reflect', 'wrap']):
         raise NotImplementedError('%s not a valid resizing mode' % mode)
 
-    # TODO Use str param for processing to specify more processing functions
-    if processing == '' or 'hu':
-        from data_manipulation_1 import get_pixels_hu as processing
-
     # Bring all dicoms in dicom_directory into memory
-    # with pixel values adjusted with processing function (defined above)
-    patient_array = processing(load_scan(dicom_directory))
+    patient_array = load_scan(dicom_directory)
+
+    # FUTURE Use str param for processing to specify more processing functions
+    if processing == '' or 'hu':
+        # adjust pixel values  with processing function
+        patient_array = get_pixels_hu(patient_array)
 
     patient_id = dicom_directory.split('/')[-1]
     label = get_label(patient_id)
@@ -65,6 +64,37 @@ def get_label(patient_id, data_dir='/nvme/stage1_data/'):
     labels_df = pd.read_csv(data_dir + 'stage1_labels.csv', index_col=0)
     label = labels_df.get_value(patient_id, 'cancer')
     return label
+
+
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    image = image.astype(np.int16)
+    image[image == -2000] = 0
+    for slice_number in range(len(slices)):
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+
+        image[slice_number] += np.int16(intercept)
+
+    return np.array(image, dtype=np.int16)
 
 
 if __name__ == '__main__':
